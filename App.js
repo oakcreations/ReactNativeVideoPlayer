96eb790be91bf9a91333182ccbe44267aa6cdec9
Pedro Carvalho <pedro.carvalho@medcel.com.br>
2018-06-18 17:02:27 -0300

    Video Player

--- App.js
+++ App.js
@@ -1,38 +1,170 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
+  View,
+  Dimensions,
   Text,
-  View
+  Animated,
+  TouchableWithoutFeedback,
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Video from 'react-native-video';
+import Atrox from './atrox.mp4'
 
-type Props = {};
-export default class App extends Component<Props> {
+import Icon from "react-native-vector-icons/dist/FontAwesome";
+import ProgressBar from "react-native-progress/Bar"
+
+function secondsToTime(time){
+  return ~~(time / 60) + ":" + (time % 60 < 10 ? "0": "") + time % 60;
+}
+
+
+export default class App extends React.Component {
+  state = {
+    buffering: true,
+    animated: new Animated.Value(0),
+    error: false,
+    paused: false,
+    progress: 0,
+    duration: 0,
+  }
+
+  handleError = meta => {
+    const { error: { code } } = meta;
+
+    let error = "Ocorreu um erro ao executar o vídeo";
+    
+    switch (code) {
+      case -11800:
+        error = "Não foi possivel carregar o vídeo";
+        break;
+    }
+
+    this.setState({
+      buffering: false,
+      error,
+    });
+  };
+
+  handleLoad = meta => {
+    meta.isBuffering && this.triggerBufferAnimation()
+
+    if(this.loopingAnimation && !meta.isBuffering){
+      this.loopingAnimation.stopAnimation();
+    }
+    console.log(meta.duration)
+    this.setState({
+      duration: meta.duration,
+      buffering: meta.isBuffering
+    })
+  }
+
+  handleLoadStart = () => {
+    this.triggerBufferAnimation();
+  }
+
+  triggerBufferAnimation = () => {
+    this.loopingAnimation = Animated.loop(
+      Animated.timing(this.state.animated, {
+        toValue: 1,
+        duration: 500,
+      })
+    ).start();
+  }
+
+  handleMainButtonTouch = () => {
+    console.log("XXXXXXXXXXxx")
+    if (this.state.progress >= 1) {
+      this.player.seek(0);
+    }
+
+    this.setState(state => {
+      return {
+        paused: !state.paused,
+      };
+    });
+  };
+
+  handleProgressPress = e => {
+    const position = e.nativeEvent.locationX;
+    const progress = (position / 250) * this.state.duration;
+    const isPlaying = !this.state.paused;
+    
+    this.player.seek(progress);
+  };
+
+  handleProgress = progress => {
+    this.setState({
+      progress: progress.currentTime / this.state.duration,
+    });
+  };
+
+  handleEnd = () => {
+    this.setState({ paused: true });
+  };
+
   render() {
+    const { width } = Dimensions.get("window");
+    const height = width * .5625;
+    const { error, buffering } = this.state;
+    // const videoUri = 'http://google.com/notavideo'
+    const videoUri = 'http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8'
+
+    const interpolatedAnimation = this.state.animated.interpolate({
+      inputRange: [0,1],
+      outputRange: ["0deg", "360deg"]
+    });
+
+    const rotateStyle = {
+      transform: [
+        {rotate: interpolatedAnimation}
+      ]
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={buffering || error  ? styles.blackBG : undefined}>
+          <Video
+            style={{ width: "100%", height }}
+            source={{ uri: videoUri }}
+            // source={Atrox}
+            resizeMode="contain"
+            ref={ref => {
+              this.player = ref;
+            }}
+            paused={this.state.paused}
+            onLoad={this.handleLoad}
+            onLoadStart={this.handleLoadStart}
+            onProgress={this.handleProgress}
+            onEnd={this.handleEnd}
+            onError={this.handleError}
+          />
+          <View style={styles.videoCover}>
+            { buffering && <Animated.View style={rotateStyle}><Icon name="circle-o-notch" size={30} color="#FFF"/></Animated.View> }
+            { error && <Icon name="exclamation-triangle" size={30} color="red" />}
+            { error && <Text color="#FFF">{error}</Text>}
+          </View>
+          <View style={styles.controls}>
+            <TouchableWithoutFeedback onPress={this.handleMainButtonTouch}>
+              <Icon name={!this.state.paused ? "pause" : "play"} size={30} color="#FFF" />
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={this.handleProgressPress}>
+              <View>
+                <ProgressBar
+                  progress={this.state.progress}
+                  color="#FFF"
+                  unfilledColor="rgba(255,255,255,.5)"
+                  borderColor="#FFF"
+                  width={250}
+                  height={20}
+                />
+              </View>
+            </TouchableWithoutFeedback>
+            <Text style={styles.duration}>
+              {secondsToTime(Math.floor(this.state.progress * this.state.duration))}
+            </Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -41,18 +173,38 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: 200,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  controls: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    height: 48,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    position: "absolute",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingHorizontal: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  mainButton: {
+    marginRight: 15,
+  },
+  duration: {
+    color: "#FFF",
+    marginLeft: 15,
+  },
+  videoCover: {
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "transparent",
+  },
+  blackBG: {
+    backgroundColor: "#000",
   },
 });
